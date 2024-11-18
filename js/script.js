fbd55118f7803ca764a84de5429a9773ee53dbe8
Otavio Augusto Queiroz Rocha Do Vale <24161@etehort.ps.sp>
2024-11-18 20:38:23 -0300

    to + perdido que cego em tiroteio

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 //revisa
-var cookie_message=document.getElementById('cookie-message');
-var cookie_btn=document.getElementById('cookie-btn');
+// var cookie_message=document.getElementById('cookie-message');
+// var cookie_btn=document.getElementById('cookie-btn');
+
+//declaração do cookie
+document.cookie="lista=true;expires=0.5;path=/";
+
+
+//que poha estamos fazendo?
+// funcoes de aceitar e nao aceitar
+function aceitaCookie(){
+    const aceita='';//dps muda
+    if(aceita==="true"){
+        //carrega os favoritos
+    }else{
+        //retira o aviso e não salva os favoritos
+        document.getElementsByClassName("cookie-message").classList.remove("hidden");
+    }
+}
